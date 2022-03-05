b34f53f76cf925773d544f66c4ca60e466e5f36b
nou-ali <noura4403@email.com>
2022-03-04 19:08:55 -0500

    added incoming expenses componenet and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
+import { Incoming } from "./components/Incoming";
+
+
 function App() {
   return (
     <div>
       <Header />
       <div className="container">
         <Balance />
+        <Incoming />
       </div>
     </div>
   );

0f62195decbffa704b6b9049418f200fe92c49cf
nou-ali <noura4403@email.com>
2022-03-05 14:42:21 -0500

    map function for transactions

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const Transactions = () => {
+  const { transactions } = useContext(GlobalContext); //instead of context.traction, will use destructuring
+
   return (
     <div>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-        Cash <span>-400</span><button className="delete-btn">X</button>
-         </li>
+        {transactions.map((transaction) => (
+          <li className="minus">
+            {transaction.text} <span>-400</span>
+            <button className="delete-btn">X</button>
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};

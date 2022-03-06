ea56f9511b584f18170d130b06de95625f29edcf
nou-ali <noura4403@email.com>
2022-03-06 16:33:32 -0500

    db is now running

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {getTransactions} = require('../controllers/transaction-control')
+const {getTransactions, addTransaction, deleteTransaction} = require('../controllers/transaction-control')
 
 router
 .route("/")
-.get(getTransactions);
+.get(getTransactions)
+.post(addTransaction);
+
+router
+  .route("/:id")
+  .delete(deleteTransaction);
 
 module.exports = router;
